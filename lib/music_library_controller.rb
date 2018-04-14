7801ee38365f22941e77d1b4f4e32f826e57a4b7
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-14 13:48:15 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -39,7 +39,7 @@ class MusicLibraryController
   end
 
   def list_songs
-    sorted_files = Song.all.sort_by {|song| song.name}
+    sorted_songs = Song.all.sort_by {|song| song.name}
     sorted_files.each_with_index do |file, index|
       puts "#{index + 1}. #{file.chomp(".mp3")}"
     end
